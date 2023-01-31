d9f586483c25ad264cd279026a21f45cecc9d779
William Bodeis <wdbodeis@gmail.com>
2023-01-30 20:09:50 -0500

    Updating the conversion file.

--- src/Convert.py
+++ src/Convert.py
@@ -5,7 +5,7 @@ import os
 class Convert():
     def __init__(self) -> None:
         self.image_directory: str = 'images/'
-        self.PDF_directory: str = 'PDFs/'
+        self.PDF_directory: str = 'pdf/'
         self.pdf_names: list[str]
 
         # Initializing functions.
@@ -13,33 +13,41 @@ class Convert():
 
     def _get_pdf_names(self):
         try:
-            self.pdf_names = (os.listdir('PDFs/'))
-        except Exception as ex:
-            print(ex)
+            self.pdf_names = (os.listdir('pdf/'))
+        except:
+            raise ImportError('No PDF files were found.')
 
     def _convert_pdf(self):
 
-        image = convert_from_path('PDFs/' + self.pdf_names[0],
-                                   poppler_path = 'poppler//Library//bin',
-                                   dpi = 700, 
-                                   first_page = 1, 
-                                   last_page = 1)
-        # file_name = 'test.png'
-        # directory = 'images//'
-        # location = directory + file_name
-        # images[0].save('images//test.png', 'PNG')
-        image[0].save(self.image_directory, 'PNG')
+        for pdf in self.pdf_names:
+            count = 0
+            image = convert_from_path('pdf/' + self.pdf_names[count],
+                                    poppler_path = 'poppler/Library/bin',
+                                    dpi = 700, 
+                                    first_page = 1, 
+                                    last_page = 1)
+            # file_name = 'test.png'
+            # directory = 'images//'
+            location = self.image_directory + str(count + 1) + '.png'
+            # image[0].save('images//test.png', 'PNG')
+            image[0].save(location, 'PNG')
 
-        # for i, image in enumerate(images):
-        #     image.save('images/', format = 'PNG')
+            # for i, image in enumerate(images):
+            #     image.save('images/', format = 'PNG')
+            count += 1
 
     
-"""
+
 if __name__ == "__main__":
-    Convert
+    try:
+        con = Convert()
+        con._convert_pdf()
+    except Exception as ex:
+        print(ex)
 """
 x = Convert()
 # x._convert_pdf()
 # x._get_pdf_names()
 x._convert_pdf()
 # print(os.listdir('images/')[0])
+"""
