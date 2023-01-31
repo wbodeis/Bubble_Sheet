f1173bf281ae92bbb0aa858a394dbdd898a46df0
William Bodeis <wdbodeis@gmail.com>
2023-01-30 23:26:10 -0500

    Making the Convert class mroe dynamic.

--- src/Convert.py
+++ src/Convert.py
@@ -1,53 +1,44 @@
+
+import os, glob
 from pdf2image import convert_from_path
-import os
+
 # https://github.com/oschwartz10612/poppler-windows
 
 class Convert():
     def __init__(self) -> None:
-        self.image_directory: str = 'images/'
-        self.PDF_directory: str = 'pdf/'
+        self.image_directory: str = 'results/'
+        self.input_directory: str = 'input/'
         self.pdf_names: list[str]
 
         # Initializing functions.
+        self._check_for_directory()
         self._get_pdf_names()
+#-----------------------------------------------------------------------------------------------------------------------
+    def _check_for_directory(self):
+        pass
 
+#-----------------------------------------------------------------------------------------------------------------------
     def _get_pdf_names(self):
         try:
-            self.pdf_names = (os.listdir('pdf/'))
+            self.pdf_names = [f for f in os.listdir(self.input_directory) if f.endswith('.pdf')]
         except:
-            raise ImportError('No PDF files were found.')
-
+            raise FileNotFoundError('No PDF files were found.')
+        
+#-----------------------------------------------------------------------------------------------------------------------
     def _convert_pdf(self):
-
-        for pdf in self.pdf_names:
-            count = 0
-            image = convert_from_path('pdf/' + self.pdf_names[count],
-                                    poppler_path = 'poppler/Library/bin',
-                                    dpi = 700, 
-                                    first_page = 1, 
-                                    last_page = 1)
-            # file_name = 'test.png'
-            # directory = 'images//'
-            location = self.image_directory + str(count + 1) + '.png'
-            # image[0].save('images//test.png', 'PNG')
+        for i in range(len(self.pdf_names)):
+            image = convert_from_path(self.input_directory + self.pdf_names[i],
+                                      poppler_path = 'poppler/Library/bin',
+                                      dpi = 700,  
+                                      last_page = 1,
+                                      thread_count = 10)
+            location = self.image_directory + str(i + 1) + '.png'
             image[0].save(location, 'PNG')
 
-            # for i, image in enumerate(images):
-            #     image.save('images/', format = 'PNG')
-            count += 1
-
-    
-
+#-----------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     try:
         con = Convert()
         con._convert_pdf()
     except Exception as ex:
         print(ex)
-"""
-x = Convert()
-# x._convert_pdf()
-# x._get_pdf_names()
-x._convert_pdf()
-# print(os.listdir('images/')[0])
-"""
